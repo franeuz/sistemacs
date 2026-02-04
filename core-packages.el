f0e4496b0ec0c1c803aa3e01e3713b16f2698fd9
Franz Bonaparta <96510220+franeuz@users.noreply.github.com>
2026-02-04 11:12:27 -0400

    Add files via upload

---
+++ core-packages.el
@@ -0,0 +1,21 @@
+;;; core-packages.el --- Gestión de paquetes -*- lexical-binding: t; -*-
+
+(require 'package)
+
+(setq package-archives
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(provide 'core-packages)
+
+
+;;; core-packages.el termina aquí.
